4a941dece8c421449b96e98127fcabffa775a5b8
erlendkvi <erlend.kvisgaard@student.nhh.no>
2023-10-30 17:00:39 +0100

    Finished method 1

---
+++ scripts/Method1.R
@@ -0,0 +1,62 @@
+#### Paralell Computing, Assignment - BAN400 ####
+
+## Method 1: The same method as in the original assignment
+
+# Loading relevant packages
+library(tidyverse)
+library(tweedie)
+library(tictoc)
+
+# Copying the solution, but including elements from the tictoc package
+
+tic.clearlog() # Making sure the log is cleared
+
+tic("Method 1") # Initiating timer
+
+simTweedieTest <-  
+  function(N){ 
+    t.test( 
+      rtweedie(N, mu=10000, phi=100, power=1.9), 
+      mu=10000 
+    )$p.value 
+  } 
+
+MTweedieTests <-  
+  function(N,M,sig){ 
+    sum(replicate(M,simTweedieTest(N)) < sig)/M 
+  } 
+
+df <-  
+  expand.grid( 
+    N = c(10,100,1000,5000, 10000), 
+    M = 1000, 
+    share_reject = NA) 
+
+for(i in 1:nrow(df)){ 
+  df$share_reject[i] <-  
+    MTweedieTests( 
+      N=df$N[i], 
+      M=df$M[i], 
+      sig=.05) 
+} 
+
+toc(log = TRUE) # Ending timer
+
+# Printing the results/log
+printTicTocLog <-
+  function() {
+    tic.log() %>%
+      unlist %>%
+      tibble(logvals = .) %>%
+      separate(logvals,
+               sep = ":",
+               into = c("Function type", "log")) %>%
+      mutate(log = str_trim(log)) %>%
+      separate(log,
+               sep = " ",
+               into = c("Seconds"),
+               extra = "drop")
+  }
+
+printTicTocLog() %>%
+  knitr::kable()
